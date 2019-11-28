087453043760aae725a24c4d4cc171a85d04d9a3
AndriyShymoniak <andryshimonyak@gmail.com>
2019-11-28 08:06:22 +0200

    rights check added

--- js/globalValues.js
+++ js/globalValues.js
@@ -1,5 +1,27 @@
 let serverUrl = 'http://localhost:8080/';
 
+let token = window.localStorage.getItem('auth_token');
+let role = window.localStorage.getItem('auth_role');
+
+$(document).ready(function (e){
+    $.ajaxSetup({
+        headers: {
+            'Authorization' : 'Bearer ' + token
+        }
+    });
+
+    if(token) {
+        var reqType = getUrlVars()["type"];
+        window.storage = {};
+        window.storage.reqTypeVar = reqType;
+        // alert(window.storage.reqTypeVar);
+        getContent();
+        rightsCheck();
+    } else {
+        rightsCheck();
+    }
+})
+
 function include(url) {
     var script = document.createElement('script');
     script.src = url;
@@ -11,4 +33,19 @@ function getUrlVars() {
         vars[key] = value;
     });
     return vars;
+}
+
+function rightsCheck(){
+    if(role != "ROLE_ADMIN"){
+        document.getElementById('adminPageLink').addEventListener('click', function(e) {
+            e.preventDefault();
+            alert('У вас немає прав адміністратора!');
+        }, false);
+    }
+    if (role != "ROLE_USER_DOCTOR" || role != "ROLE_USER_PATIENT"){
+        document.getElementById('individualPageLink').addEventListener('click', function(e) {
+            e.preventDefault();
+            alert('Залогуйтесь, щоб зайти в особистий кабінет!');
+        }, false);
+    }
 }
