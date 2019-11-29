fb948d901968a452e0b095022ebf43b739d88455
AndriyShymoniak <andryshimonyak@gmail.com>
2019-11-29 12:40:40 +0200

    minor changes + patient page error occured

--- js/globalValues.js
+++ js/globalValues.js
@@ -2,6 +2,7 @@ let serverUrl = 'http://localhost:8080/';
 
 let token = window.localStorage.getItem('auth_token');
 let role = window.localStorage.getItem('auth_role');
+let id = window.localStorage.getItem('auth_id');
 
 $(document).ready(function (e){
     $.ajaxSetup({
@@ -17,6 +18,7 @@ $(document).ready(function (e){
         // alert(window.storage.reqTypeVar);
         rightsCheck();
         changeLoginIfLogged();
+        accessToIndividualPage();
     } else {
         rightsCheck();
         changeLoginIfUnlogged();
@@ -24,12 +26,19 @@ $(document).ready(function (e){
 })
 
 $(document).ready(function(){
-    $('#loginLink').on('click', function(){
-        window.localStorage.removeItem('auth_token');
-        window.localStorage.removeItem('auth_role');
-        $(location).attr('href', 'index.html');
+    $('#loginLink').on('click', function(e){
+        e.preventDefault();
+        if (token == null){
+            $(location).attr('href', 'sign_in.html');
+        } else {
+            window.localStorage.removeItem('auth_token');
+            window.localStorage.removeItem('auth_role');
+            window.localStorage.removeItem('auth_id');
+            $(location).attr('href', 'index.html');
+        }
     })
 })
+
 
 
 function include(url) {
@@ -37,6 +46,7 @@ function include(url) {
     script.src = url;
     document.getElementsByTagName('head')[0].appendChild(script);
 }
+
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -52,7 +62,7 @@ function rightsCheck(){
             alert('У вас немає прав адміністратора!');
         }, false);
     }
-    if (role != "ROLE_USER_DOCTOR" || role != "ROLE_USER_PATIENT"){
+    if (role != "ROLE_USER_DOCTOR" && role != "ROLE_USER_PATIENT"){
         document.getElementById('individualPageLink').addEventListener('click', function(e) {
             e.preventDefault();
             alert('Залогуйтесь, щоб зайти в особистий кабінет!');
@@ -66,4 +76,18 @@ function changeLoginIfLogged() {
 
 function changeLoginIfUnlogged() {
     $('#loginLink').text("Увійти");
+}
+
+function accessToIndividualPage() {
+    $('#individualPageLink').on('click', function (e) {
+        e.preventDefault();
+
+        if (role == "ROLE_USER_DOCTOR"){
+            $(location).attr('href', 'doctor_page.html?doctor_id=' + id);
+        } else if (role == "ROLE_USER_PATIENT"){
+            $(location).attr('href', 'patient_page.html?patient_id=' + id);
+        }
+
+        // $(location).attr('href', 'http://stackoverflow.com');
+    })
 }
